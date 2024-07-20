ee2d48bcb5eb449ad375dc9fb380ea0951501e72
itsjeetjoshi <jeet.h.joshi@gmail.com>
2024-07-20 20:23:33 +0530

    New updates to code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:soundcircle/Main%20page.dart';
+import 'package:soundcircle/gradientText.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,85 +32,43 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.of(context)
+          .pushReplacement(MaterialPageRoute(builder: (_) => mainPage()));
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Expanded(
-        child: Center(
-          child: FittedBox(
-            fit: BoxFit.fitWidth,
-            child: Image.asset('assets/Create account page.png'),
+        body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xFF7185c1), Color(0xFF494f66), Color(0xFF292347)],
           ),
         ),
-      ),
+        child: const Center(
+          child: const GradientText(
+            'SoundCircle',
+            gradient: LinearGradient(
+              colors: [
+                Color(0xFF43309a), // Your start color
+                Color(0xFF7477cc),
+                Color(0xFFa4bbfb),// Your end color
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            style: TextStyle(fontSize: 40, fontWeight: FontWeight.w400),
+          ),
+        ),
+      )
     );
   }
 }
-/*Container(
-        height: constraints.maxHeight,
-        width: constraints.maxWidth,
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            colors: [
-              Color(0xFFa8c0ff),
-              Color(0xFF3f2b96),
-            ],
-          ),
-        ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              const SizedBox(height: 150), // Add top margin
-              const SizedBox(
-                height: 100,
-                child: Text(
-                  "Log-in With",
-                  style: TextStyle(
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold,
-                    color: Color(0xFF3f2b96),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20), // Add spacing between widgets
-              ElevatedButton(
-                style: ButtonStyle(
-                  minimumSize: MaterialStateProperty.all(Size(75,50)),
-                  backgroundColor: MaterialStateProperty.all(Colors.deepPurpleAccent),
-                  shadowColor: MaterialStateProperty.all(Colors.blueGrey),
-                ),
-                onPressed: null,
-                child: const Text(
-                    "Phone Number",
-                  style: TextStyle(
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20), // Add spacing between widgets
-              ElevatedButton(
-                style: ButtonStyle(
-                  minimumSize: MaterialStateProperty.all(Size(150,50)),
-                  backgroundColor: MaterialStateProperty.all(Colors.deepPurpleAccent),
-                  shadowColor: MaterialStateProperty.all(Colors.black),
-                ),
-                onPressed: null,
-                child: const Text(
-                    "Email-Id",
-                  style: TextStyle(
-                    color: Colors.black
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
- */
+
+
