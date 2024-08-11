fe6a80e9d00d477b462b591a4e346c1c0ce0a108
itsjeetjoshi <jeet.h.joshi@gmail.com>
2024-08-11 20:08:32 +0530

    new changes

--- lib/feed.dart
+++ lib/feed.dart
@@ -19,134 +19,130 @@ class _feedState extends State<feed> {
       _showProfileCard = !_showProfileCard;
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              Color(0xFF7185c1),
-              Color(0xFF494f66),
-              Color(0xFF292347),
-            ],
-          ),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              const SizedBox(height: 30),
-              const GradientText(
-                'SoundCircle',
-                gradient: LinearGradient(
-                  colors: [
-                    Color(0xFF43309a), // Start color
-                    Color(0xFF7477cc),
-                    Color(0xFFa4bbfb), // End color
-                  ],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                ),
-                style: TextStyle(fontSize: 40, fontWeight: FontWeight.w400),
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color(0xFF7185c1),
+                  Color(0xFF494f66),
+                  Color(0xFF292347),
+                ],
               ),
-              const SizedBox(height: 20),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: 5, // Number of profile cards
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                      onTap: () {
-                        profileCard(
-
-                        );
-                        print('Card $index tapped');
-                      },
-                      child: Card(
-                        margin: const EdgeInsets.symmetric(vertical: 10),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15),
-                        ),
-                        elevation: 5,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Row(
-                            children: [
-                              // ClipRRect(
-                              //   borderRadius: BorderRadius.circular(10),
-                              //   child: Image.network(
-                              //     'https://via.placeholder.com/100', // Placeholder image
-                              //     width: 100,
-                              //     height: 100,
-                              //     fit: BoxFit.cover,
-                              //   ),
-                              // ),
-                              const SizedBox(width: 10),
-                              Expanded(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      'User ${index + 1}',
-                                      style: TextStyle(
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.bold,
-                                      ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 30),
+                  const GradientText(
+                    'SoundCircle',
+                    gradient: LinearGradient(
+                      colors: [
+                        Color(0xFF43309a),
+                        Color(0xFF7477cc),
+                        Color(0xFFa4bbfb),
+                      ],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                    ),
+                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.w400),
+                  ),
+                  const SizedBox(height: 20),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: 5, // Number of profile cards
+                      itemBuilder: (context, index) {
+                        return GestureDetector(
+                          onTap: () {
+                            _toggleProfileCard();
+                            print('Card $index tapped');
+                          },
+                          child: Card(
+                            margin: const EdgeInsets.symmetric(vertical: 10),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            elevation: 5,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                children: [
+                                  const SizedBox(width: 10),
+                                  Expanded(
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          'User ${index + 1}',
+                                          style: TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        Text(
+                                          'Age ${20 + index}',
+                                          style: TextStyle(
+                                            fontSize: 16,
+                                            color: Colors.grey[600],
+                                          ),
+                                        ),
+                                        const SizedBox(height: 5),
+                                        Text(
+                                          'This is a short bio of the user. It gives a brief introduction.',
+                                          style: TextStyle(
+                                            fontSize: 14,
+                                            color: Colors.grey[700],
+                                          ),
+                                          overflow: TextOverflow.ellipsis,
+                                          maxLines: 2,
+                                        ),
+                                      ],
                                     ),
-                                    Text(
-                                      'Age ${20 + index}',
-                                      style: TextStyle(
-                                        fontSize: 16,
-                                        color: Colors.grey[600],
-                                      ),
-                                    ),
-                                    const SizedBox(height: 5),
-                                    Text(
-                                      'This is a short bio of the user. It gives a brief introduction.',
-                                      style: TextStyle(
-                                        fontSize: 14,
-                                        color: Colors.grey[700],
-                                      ),
-                                      overflow: TextOverflow.ellipsis,
-                                      maxLines: 2,
-                                    ),
-                                  ],
-                                ),
+                                  ),
+                                  IconButton(
+                                    icon: const Icon(Icons.thumb_up_alt),
+                                    onPressed: () {
+                                      // Handle like action
+                                    },
+                                  ),
+                                  IconButton(
+                                    icon: const Icon(Icons.clear),
+                                    onPressed: () {
+                                      // Handle pass action
+                                    },
+                                  ),
+                                ],
                               ),
-                              IconButton(
-                                icon: Icon(Icons.thumb_up_alt),
-                                onPressed: () {
-                                  // Handle like action
-                                },
-                              ),
-                              IconButton(
-                                icon: Icon(Icons.clear),
-                                onPressed: () {
-                                  // Handle pass action
-                                },
-                              ),
-                            ],
+                            ),
                           ),
-                        ),
-                      ),
-                    );
-                  },
-                ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
+          if (_showProfileCard)
+            buildOverlay(), // Display overlay if _showProfileCard is true
+        ],
       ),
     );
   }
-  Widget _buildOverlay() {
+
+  Widget buildOverlay() {
     return Positioned.fill(
       child: Stack(
         children: [
-          // Blurred background
           GestureDetector(
             onTap: _toggleProfileCard,
             child: BackdropFilter(
@@ -156,7 +152,6 @@ class _feedState extends State<feed> {
               ),
             ),
           ),
-          // Profile card in the center
           Center(
             child: profileCard(),
           ),
