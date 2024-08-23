eddd356e7e6c5eac8beb0f0719964bcfaaf70bbb
itsjeetjoshi <jeet.h.joshi@gmail.com>
2024-08-23 17:55:53 +0530

    backend changes

---
+++ API/api.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors');
+const app = express()
+const port = 3000
+app.use(cors())
+const connection = require('../Connection/conn')
+app.get("/User", (req,res)=>{
+    connection.query("select * from User", (err, rows, fields) => {
+        if (err) throw err
+        res.send(200, {
+            userName: rows[0].userName
+        })
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Successfully listening on port ${port}`)
+})
---
+++ Connection/conn.js
@@ -0,0 +1,22 @@
+// Import mysql2
+const mysql = require('mysql2');
+
+// Create a connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '01bhai04',
+  database: 'SoundCircle'
+});
+
+// Connect to the database
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting: ' + err.stack);
+    return;
+  }
+  console.log('Connected as id ' + connection.threadId);
+});
+
+// Export the connection
+module.exports = connection;
