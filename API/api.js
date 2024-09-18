fe7891cb4359e7ac9acc314a353a15de33b880d0
itsjeetjoshi <jeet.h.joshi@gmail.com>
2024-09-18 23:13:02 +0530

    backend changes

--- API/api.js
+++ API/api.js
@@ -1,16 +1,201 @@
 const express = require('express')
 const cors = require('cors')
+const axios = require('axios')
 const app = express()
 const port = 3000
 app.use(cors())
+app.use(express.json())
 const connection = require('../Connection/conn')
-app.get("/User", (req,res)=>{
-    connection.query("select * from User", (err, rows, fields) => {
+app.post("/getFeed", async (req, res) => {
+    try {
+        const rows = await new Promise((resolve, reject) => {
+            connection.query("select * from User", (err, rows) => {
+                if (err) return reject(err);
+                resolve(rows);
+            });
+        });
+        const possibleConnectionsList = await possibleConnections(req.body.userId);
+        var possibleConnectionsUserIds = []
+        var connectionText = []
+        var percentages = []
+        for(let data of possibleConnectionsList){
+            possibleConnectionsUserIds.push(data.userId)
+            connectionText.push(data.text)
+            percentages.push(data.percentage)
+        }
+        var data = {
+            "userIds": possibleConnectionsUserIds
+        }
+        var response = await axios.post("http://localhost:3000/getUserById", data)
+        var feedData = response.data
+        for(var i in feedData){
+            feedData[i].text = connectionText[i]
+            feedData[i].percentage = percentages[i]
+        }
+        res.send(200, feedData);
+    } catch (err) {
+        console.error(err);
+        res.send(500, "An error occurred while processing the request.");
+    }
+});
+app.post("/getUserById", (req, res) => {
+    var userIds = req.body.userIds
+    connection.query("select * from user where userId in (?)", [userIds], (err, result) => {
         if (err) throw err
-        res.send(200, rows)
+        res.send(200, result)
     })
 })
+app.post("/createUser", (req,res)=>{
+    connection.query(`insert into user(userName, email, phoneNo, age, gender, sentLikes, recievedLikes, connections) values('${req.body.username}', '${req.body.email}', '${req.body.phoneNo}', '${req.body.age}', '${req.body.gender}', '', '', '')`, (err) => {
+        if (err) throw err
+        res.send(200, "ok");
+    })
+})
+app.post("/addGenreArtist", (req, res) => {
+    var genres = req.body.genres;
+    var artists = req.body.artists;
+    var genresString = genres.join(', ') + ', ';
+    var artistsString = artists.join(', ') + ', ';
 
+    connection.query("select userId from user", (err, result) => {
+        if (err) throw err;
+        if (result.length > 0) {
+            var userId = result[0].userId;
+            connection.query(`insert into userPreference(userId, genres, artists) values(${userId}, '${genresString}', '${artistsString}')`, (err) => {
+                if (err) throw err;
+                res.status(200).send("ok");
+            });
+        } else {
+            res.status(404).send("User not found");
+        }
+    });
+});
+app.post("/addLike", (req, res) => {
+    connection.query(`update user set sentLikes = concat(sentLikes, '${req.body.userId}, ') where userId = ${req.body.currentUserId}`, (err) => {
+        if (err) throw err
+        res.send(200, "ok")
+    })
+})
+app.post("/getCurrentUserId", (req, res) => {
+    connection.query(`select userId from user where phoneNo = '${req.body.phoneNo}'`, (err, result) => {
+        if (err) throw err
+        res.send(200, result)
+    })
+})
 app.listen(port, () => {
     console.log(`Successfully listening on port ${port}`)
 })
+function possibleConnections(currentUserId){
+    return new Promise((resolve, reject) => {
+        var currentUser
+        var otherUsers = []
+        var currentUserPreference
+        console.log(currentUserId)
+        connection.query("select * from userPreference", (err, result) => {
+            if (err) throw err
+            for(let res of result){
+                if(res.userId == currentUserId){
+                    currentUserPreference = res
+                }
+            }
+        })
+        connection.query(`select * from user`, (err, result) => {
+            if (err) throw err
+            for(let res of result){
+                if(res.userId == currentUserId){
+                    currentUser = res
+                }
+                else{
+                    otherUsers.push(res)
+                }
+            }
+            console.log(currentUser)
+            var existingLikesString = currentUser.sentLikes || ''
+            var existingConnectionsString = currentUser.connections || ''
+            var recievedLikesString = currentUser.recievedLikes || ''
+            var possibleConnectionsList = []
+            if(existingLikesString!='' && existingConnectionsString!='' && recievedLikesString!=''){
+                var existingLikes = existingLikesString.split(", ")
+                var existingConnections = existingConnectionsString.split(", ")
+                var recievedLikes = recievedLikesString.split(", ")
+                for(var i=0; i<otherUsers.length; i++){
+                    if((otherUsers[i].userId in existingLikes) || (otherUsers[i].userId in existingConnections) || (otherUsers[i].userId in recievedLikes)){
+                        continue
+                    }
+                    else{
+                        possibleConnectionsList.push(otherUsers[i].userId)
+                    }
+                }
+            }
+            else{
+                possibleConnectionsList = otherUsers
+            }
+            var possibleConnectionsList = checkMusicTaste(currentUserPreference.genres, currentUserPreference.artists, possibleConnectionsList)
+            resolve(possibleConnectionsList)
+        })
+    })
+}
+function checkMusicTaste(currentUserGenres, currentUserArtists, otherUsers) {
+    var currentUserGenresList = currentUserGenres.split(", ");
+    var currentUserArtistsList = currentUserArtists.split(", ");
+    var userMusicTasteMatch = [];
+
+    for (let user of otherUsers) {
+        var otherUserGenresList = user.genres.split(", ");
+        var otherUserArtistsList = user.artists.split(", ");
+
+        var commonGenres = [];
+        var commonArtists = [];
+
+        var commonGenresString = ''
+        var commonArtistsString = ''
+
+        var artistText = '';
+        var percentage = 3;
+
+        for (let genre of otherUserGenresList) {
+            if (currentUserGenresList.includes(genre)) {
+                commonGenres.push(genre);
+                commonGenresString = commonGenresString + " " + genre
+            }
+        }
+
+        for (let artist of otherUserArtistsList) {
+            if (currentUserArtistsList.includes(artist)) {
+                commonArtists.push(artist);
+                commonArtistsString = commonArtistsString + " " + artist
+            }
+        }
+        if (commonGenres.length > 0 || commonArtists.length > 0) {
+            // Calculate percentage based on the new equation
+            let genrePercentage = (commonGenres.length / 3) * 30; // 30% weight for genres
+            let artistPercentage = (commonArtists.length / 9) * 70; // 70% weight for artists
+        
+            let percentage = genrePercentage + artistPercentage;
+        
+            // Form genre text
+            let genreText = '';
+            if (commonGenres.length === 1) {
+                genreText = ` ${commonGenres[0]} music`;
+            } else if (commonGenres.length === 2) {
+                genreText = ` ${commonGenres[0]} and ${commonGenres[1]} music`;
+            } else if (commonGenres.length > 2) {
+                genreText = ` ${commonGenres.slice(0, -1).join(', ')}, and ${commonGenres[commonGenres.length - 1]} music`;
+            }
+        
+            // Form artist text
+            let artistText = '';
+            if (commonArtists.length > 0) {
+                artistText = ` and have common favourite artists ${commonArtists.join(', ')}`;
+            }
+        
+            // Push result
+            userMusicTasteMatch.push({
+                "text": `You both listen to${genreText}${artistText}`,
+                "percentage": percentage,
+                "userId": user.userId
+            });
+        }
+    }
+    return userMusicTasteMatch;
+}
--- Admin/index.js
+++ Admin/index.js
@@ -10,20 +10,46 @@ document.addEventListener("DOMContentLoaded", function() {
             })
             .then((data) => {
                 const jsonData = data;
-                console.log(jsonData['age'])
+                console.log(jsonData)
                 const userDataDiv = document.getElementById("data");
-                if (userDataDiv) {
+                const table1 = document.getElementById("table1");
+                table1.style.display = "table";
+                /*if (userDataDiv) {
                     const userData = `<p>Name: ${jsonData.userName}</p><p>Age: ${jsonData.age}</p><p>Gender: ${jsonData.gender}</p>`;
                     userDataDiv.innerHTML = userData;
                 } else {
                     console.error('Element with ID "data" not found.');
-                }
-                const userName = document.getElementById("userName");
-                const age = document.getElementById("age");
-                const gender = document.getElementById("gender");
-                userName.innerHTML = jsonData.userName;
-                age.innerHTML = jsonData.age;
-                gender.innerHTML = jsonData.gender
+                }*/
+                const userName0 = document.getElementById("userName0");
+                const age0 = document.getElementById("age0");
+                const gender0 = document.getElementById("gender0");
+                const userName1 = document.getElementById("userName1");
+                const age1 = document.getElementById("age1");
+                const gender1 = document.getElementById("gender1");
+                const userName2 = document.getElementById("userName2");
+                const age2 = document.getElementById("age2");
+                const gender2 = document.getElementById("gender2");
+                const userName3 = document.getElementById("userName3");
+                const age3 = document.getElementById("age3");
+                const gender3 = document.getElementById("gender3");
+                const userName4 = document.getElementById("userName4");
+                const age4 = document.getElementById("age4");
+                const gender4 = document.getElementById("gender4");
+                userName0.innerHTML = jsonData[0].userName;
+                age0.innerHTML = jsonData[0].age;
+                gender0.innerHTML = jsonData[0].gender
+                userName1.innerHTML = jsonData[1].userName;
+                age1.innerHTML = jsonData[1].age;
+                gender1.innerHTML = jsonData[1].gender
+                userName2.innerHTML = jsonData[2].userName;
+                age2.innerHTML = jsonData[2].age;
+                gender2.innerHTML = jsonData[2].gender
+                userName3.innerHTML = jsonData[3].userName;
+                age3.innerHTML = jsonData[3].age;
+                gender3.innerHTML = jsonData[3].gender
+                userName4.innerHTML = jsonData[4].userName;
+                age4.innerHTML = jsonData[4].age;
+                gender4.innerHTML = jsonData[4].gender
                 // Use template literals correctly
                 //const userData = `<p>Name: ${jsonData.userName}</p><p>Age: ${jsonData.age}</p><p>Gender: ${jsonData.gender}</p>`;
                 //userDataDiv.innerHTML = userData;
